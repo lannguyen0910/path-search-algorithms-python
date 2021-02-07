ec3de4b359aa41c7a758d9ad05b05326d54488f2
lannguyen <18120051@student.hcmus.edu.vn>
2021-02-08 02:14:49 +0700

    add Node class

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # path-search-algorithms-python
-Implement of different path search algorithms like BFS, DFS, Dijkstra, A* in Python
+Implement of different path search algorithms like BFS, DFS, Dijkstra, A* on a squared grid in Python
---
+++ node.py
@@ -0,0 +1,18 @@
+class Node():
+    def __init__(self, row, col):
+        self.row = row
+        self.col = col
+        self.val = None
+        self.isVisited = False
+        self.isWall = False
+        self.prev = None
+        self.distance = float('inf')
+
+    def __eq__(self, other) -> bool:
+        return self.row == other.row and self.col == other.col
+
+    def __lt__(self, other):
+        return self.distance < other.distance
+
+    def __gt__(self, other):
+        return self.distance > other.distance
